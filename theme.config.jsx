b1765043ee021caac183db3981ccaa88c29aa025
Philip Slisenko <slisenkophilip@gmail.com>
2024-02-29 13:56:59 -0700

    init

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+const config = {
+  logo: <span></span>,
+  project: {
+    link: "https://github.com/PhilipSlisenko",
+  },
+  gitTimestamp: null,
+  footer: {
+    component: null,
+  },
+  search: {
+    component: null,
+  },
+};
+
+export default config;
